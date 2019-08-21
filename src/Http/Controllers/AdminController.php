674f6ceae03e70a19e4f85698875b5e2e60517b0
Molitor István <istvanmolitor@gmail.com>
2019-08-21 22:29:10 +0200

    index

---
+++ src/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace IstvanMolitor\LaravelAdmin\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        dd('pppp');
+    }
+}
--- src/Providers/LaravelAdminServiceProvider.php
+++ src/Providers/LaravelAdminServiceProvider.php
@@ -8,6 +8,8 @@ class LaravelAdminServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'admin');
     }
 
     public function register()
