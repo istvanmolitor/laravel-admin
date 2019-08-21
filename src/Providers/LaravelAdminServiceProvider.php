9f0e3119e3f644afa3a72ad1a435ba54b6a380ff
Molitor István <istvanmolitor@gmail.com>
2019-08-21 21:53:57 +0200

    admin init

---
+++ src/Providers/LaravelAdminServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace IstvanMolitor\LaravelAdmin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAdminServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
