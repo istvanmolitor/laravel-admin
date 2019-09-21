5681ad7f7d13b295be7615158b46282ac1fae812
Molitor István <istvanmolitor@gmail.com>
2019-09-21 13:09:17 +0200

    Form mezők

--- src/Providers/LaravelAdminServiceProvider.php
+++ src/Providers/LaravelAdminServiceProvider.php
@@ -10,6 +10,10 @@ class LaravelAdminServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'admin');
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => base_path('resources/views/vendor/admin'),
+        ]);
     }
 
     public function register()
---
+++ src/resources/views/form/email.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+    {!!  Form::label($name, $label, ['class' => 'control-label']) !!}
+    {!! Form::email($name, '', ['class' => 'form-control']) !!}
+</div>
